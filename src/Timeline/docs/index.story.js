3ae72e8b923d12383ae7d2a2723d7b190c1f7310
adimo <adimo@wix.com>
2020-04-20 19:15:39 +0300

    story

--- src/Timeline/docs/index.story.js
+++ src/Timeline/docs/index.story.js
@@ -15,7 +15,6 @@ import {
 } from 'wix-storybook-utils/Sections';
 
 import TextButton from '../../TextButton';
-import Time from 'wix-ui-icons-common/Time';
 
 import { storySettings } from '../test/storySettings';
 import allComponents from '../../../stories/utils/allComponents';
@@ -90,9 +89,13 @@ export default {
             title: 'Simple Usage',
             text: 'A simple example with compact preview',
             source:
-              '<Timeline items={[{' +
-              "    label: 'Quote #8 Accepted: Website setup, $7.00',\n" +
-              "    suffix: 'Jan 1, 2019 12:03 AM',\n" +
+              '<Timeline items={[\n' +
+              '  {\n' +
+              '    label:\n' +
+              "      'New Order: 1 item, $4.99 this will be a longer text that will be two lines or a very very long text for a vertical that needs a lot of text that will be three lines',\n" +
+              '    suffix: <TextButton weight="normal" size="small">\n' +
+              '      suffix button\n' +
+              '    </TextButton>,\n' +
               '    labelAction: (\n' +
               '      <TextButton weight="normal" size="small">\n' +
               '        View Order\n' +
@@ -100,14 +103,40 @@ export default {
               '    ),\n' +
               '  },\n' +
               '  {\n' +
-              "    label: 'Quote #8 Accepted: Website setup, $7.00',\n" +
+              '    label:\n' +
+              "      'New Order: 1 item, $4.99 this will be a longer text that will be two lines or a very very long text for a vertical that needs a lot of text that will be three lines',\n" +
               "    suffix: 'Jan 1, 2019 12:03 AM',\n" +
               '    labelAction: (\n' +
               '      <TextButton weight="normal" size="small">\n' +
               '        View Order\n' +
               '      </TextButton>\n' +
               '    ),\n' +
-              '  }]}/>',
+              '    customPrefix: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12.5,20 C8.35786438,20 5,16.6421356 5,12.5 C5,8.35786438 8.35786438,5 12.5,5 C16.6421356,5 20,8.35786438 20,12.5 C20,16.6421356 16.6421356,20 12.5,20 Z M12.5,19 C16.0898509,19 19,16.0898509 19,12.5 C19,8.91014913 16.0898509,6 12.5,6 C8.91014913,6 6,8.91014913 6,12.5 C6,16.0898509 8.91014913,19 12.5,19 Z M13,12.2928932 L15.3535534,14.6464466 L14.6464466,15.3535534 L12,12.7071068 L12,9 L13,9 L13,12.2928932 Z"/></svg>,\n' +
+              '  },\n' +
+              '  {\n' +
+              '    id: 1,\n' +
+              "    label: 'Quote #8 Accepted: Website setup, $7.00',\n" +
+              '  },\n' +
+              '  {\n' +
+              '    label:\n' +
+              "      'New Order: 1 item, $4.99 this will be a longer text that will be two lines or a very very long text for a vertical that needs a lot of text that will be three lines',\n" +
+              '    labelAction: (\n' +
+              '      <TextButton weight="normal" size="small">\n' +
+              '        View Order\n' +
+              '      </TextButton>\n' +
+              '    ),\n' +
+              '  },\n' +
+              '  {\n' +
+              '    label:\n' +
+              "      'New Order: 1 item, $4.99 this will be a longer text that will be two lines or a very very long text for a vertical that needs a lot of text that will be three lines',\n" +
+              "    suffix: 'Jan 1, 2019 12:03 AM',\n" +
+              '    labelAction: (\n' +
+              '      <TextButton weight="normal" size="small">\n' +
+              '        View Order\n' +
+              '      </TextButton>\n' +
+              '    ),\n' +
+              '  },\n' +
+              ']}/>',
           }),
 
           code({
@@ -128,51 +157,50 @@ export default {
 };
 
 // [
-// //   {
-// //     label: 'Quote #8 Accepted: Website setup, $7.00',
-// //     suffix: 'Jan 1, 2019 12:03 AM',
-// //     labelAction: (
-// //       <TextButton weight="normal" size="small">
-// //         View Order
-// //       </TextButton>
-// //     ),
-// //   },
-// //   {
-// //     label:
-// //       'New Order: 1 item, $4.99 this will be a longer text that will be two lines or a very very long text for a vertical that needs a lot of text that will be three lines',
-// //     suffix: 'Jan 1, 2019 12:03 AM',
-// //     labelAction: (
-// //       <TextButton weight="normal" size="small">
-// //         View Order
-// //       </TextButton>
-// //     ),
-// //     prefix: <Time />,
-// //   },
-// //   {
-// //     label: 'Quote #8 Accepted: Website setup, $7.00',
-// //     labelAction: (
-// //       <TextButton weight="normal" size="small">
-// //         View Order
-// //       </TextButton>
-// //     ),
-// //   },
-// //   {
-// //     label:
-// //       'New Order: 1 item, $4.99 this will be a longer text that will be two lines or a very very long text for a vertical that needs a lot of text that will be three lines',
-// //     suffix: 'Jan 1, 2019 12:03 AM',
-// //   },
-// //   {
-// //     label:
-// //       'New Order: 1 item, $4.99 this will be a longer text that will be two lines or a very very long text for a vertical that needs a lot of text that will be three lines',
-// //   },
-// //   {
-// //     label:
-// //       'New Order: 1 item, $4.99 this will be a longer text that will be two lines or a very very long text for a vertical that needs a lot of text that will be three lines',
-// //     suffix: 'Jan 1, 2019 12:03 AM',
-// //     labelAction: (
-// //       <TextButton weight="normal" size="small">
-// //         View Order
-// //       </TextButton>
-// //     ),
-// //   },
-// // ]
+//   {
+//     label:
+//       'New Order: 1 item, $4.99 this will be a longer text that will be two lines or a very very long text for a vertical that needs a lot of text that will be three lines',
+//     suffix: <TextButton weight="normal" size="small">
+//       suffix button
+//     </TextButton>,
+//     labelAction: (
+//       <TextButton weight="normal" size="small">
+//         View Order
+//       </TextButton>
+//     ),
+//   },
+//   {
+//     label:
+//       'New Order: 1 item, $4.99 this will be a longer text that will be two lines or a very very long text for a vertical that needs a lot of text that will be three lines',
+//     suffix: 'Jan 1, 2019 12:03 AM',
+//     labelAction: (
+//       <TextButton weight="normal" size="small">
+//         View Order
+//       </TextButton>
+//     ),
+//     customPrefix: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12.5,20 C8.35786438,20 5,16.6421356 5,12.5 C5,8.35786438 8.35786438,5 12.5,5 C16.6421356,5 20,8.35786438 20,12.5 C20,16.6421356 16.6421356,20 12.5,20 Z M12.5,19 C16.0898509,19 19,16.0898509 19,12.5 C19,8.91014913 16.0898509,6 12.5,6 C8.91014913,6 6,8.91014913 6,12.5 C6,16.0898509 8.91014913,19 12.5,19 Z M13,12.2928932 L15.3535534,14.6464466 L14.6464466,15.3535534 L12,12.7071068 L12,9 L13,9 L13,12.2928932 Z"/></svg>,
+//   },
+//   {
+//     id: 1,
+//     label: 'Quote #8 Accepted: Website setup, $7.00',
+//   },
+//   {
+//     label:
+//       'New Order: 1 item, $4.99 this will be a longer text that will be two lines or a very very long text for a vertical that needs a lot of text that will be three lines',
+//     labelAction: (
+//       <TextButton weight="normal" size="small">
+//         View Order
+//       </TextButton>
+//     ),
+//   },
+//   {
+//     label:
+//       'New Order: 1 item, $4.99 this will be a longer text that will be two lines or a very very long text for a vertical that needs a lot of text that will be three lines',
+//     suffix: 'Jan 1, 2019 12:03 AM',
+//     labelAction: (
+//       <TextButton weight="normal" size="small">
+//         View Order
+//       </TextButton>
+//     ),
+//   },
+// ]
