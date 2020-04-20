afceff36a2b5425c62b56b339cc50cdf0cb2d6e3
adimo <adimo@wix.com>
2020-04-20 16:13:34 +0300

    typescript interface

--- src/Timeline/index.d.ts
+++ src/Timeline/index.d.ts
@@ -1,9 +1,16 @@
 import * as React from 'react';
 
+export interface TimelineItem {
+  id: string;
+  label: string;
+  labelAction?: React.ReactNode;
+  customPrefix?: React.ReactNode;
+  suffix?: React.ReactNode | string;
+}
+
 export interface TimelineProps {
   dataHook?: string;
-  className?: string;
-  buttonText?: string;
+  items: TimelineItem[];
 }
 
 export default class Timeline extends React.PureComponent<TimelineProps>{}
