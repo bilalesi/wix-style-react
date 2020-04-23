f9b9b8d1e576df872bbef286e2f3cd5f4946c212
adimo <adimo@wix.com>
2020-04-23 17:24:11 +0300

    visual tests

--- src/Timeline/test/Timeline.visual.js
+++ src/Timeline/test/Timeline.visual.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Timeline from '../Timeline';
+import TextButton from '../../TextButton';
+import Time from 'wix-ui-icons-common/Time';
 
 const commonProps = {
   // use for repeated props across the tests (e.g. {buttonText: 'example'})
@@ -11,9 +13,60 @@ const tests = [
     describe: 'sanity', // prop name (e.g. size)
     its: [
       {
-        it: 'default', // prop variation (e.g. small)
+        it: 'simple', // prop variation (e.g. small)
         props: {
-          // the simulation (e.g. {size: "small"})
+          items: [
+            {
+              label: 'simple text',
+            },
+            {
+              label:
+                'some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text some long text',
+              suffix: 'suffix text',
+            },
+          ],
+        },
+      },
+      {
+        it: 'custom suffix', // prop variation (e.g. small)
+        props: {
+          items: [
+            {
+              label: 'simple text',
+            },
+            {
+              label: 'simple text',
+              suffix: <TextButton>suffix link button</TextButton>,
+            },
+          ],
+        },
+      },
+      {
+        it: 'label action', // prop variation (e.g. small)
+        props: {
+          items: [
+            {
+              label: 'simple text',
+            },
+            {
+              label: 'simple text',
+              labelAction: <TextButton>label action button</TextButton>,
+            },
+          ],
+        },
+      },
+      {
+        it: 'custom prefix', // prop variation (e.g. small)
+        props: {
+          items: [
+            {
+              label: 'simple text',
+            },
+            {
+              label: 'simple text',
+              customPrefix: <Time />,
+            },
+          ],
         },
       },
     ],
